1e0c6ecdc891b2ce0a48e9bfd28824f987604c2c
lynxforge <lynnxioray@gmail.com>
2026-01-29 14:08:20 +0530

    multiplayer attempt

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,4 +1,13 @@
 import random
+import socket
+
+def get_random_word():
+    words = ["apple", "grape", "plane", "tiger", "house", "river",
+             "brick", "snake", "chair", "smile", "bread", "light",
+             "stone", "cloud", "music", "beach", "train", "grass"
+    ]
+    return random.choice(words)
+
 stages = [r'''
   +---+
   |   |
@@ -55,27 +64,128 @@ stages = [r'''
       |
 =========
 ''']
-word_list = ["Happy", "baboon", "camel"]
 
+
+# =======Mode Selection ============
+
+print("1. Single Player")
+print("2. Multiplayer (Same Wifi)")
+mode = input("Choose mode: ")
+
+# ======= Multiplayer ============== 
+
+def multiplayer_host(chosen_word):
+    host = ""
+    port = 5555
+
+    server = socket.socket()
+    server.bind((host, port))
+    server.listen(1)
+
+    print("Waiting for another player to join...")
+    conn, addr = server.accept()
+    print("Player joined: ", addr)
+
+    lives = 6
+    correct_letters = []
+    guessed_letters = []
+
+    while lives > 0:
+        display = ""
+        for letter in  chosen_word:
+            if letter in correct_letters:
+                display += letter
+            else:
+                display += "_"
+
+        conn.send(f"{display}|{lives}".encode())
+
+        if "_" not in  display:
+            conn.send(b"WIN")
+            break
+
+        guess = conn.recv(1024).decode().lower()
+
+        if guess in guessed_letters:
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess in chosen_word:
+            if guess not in correct_letters:
+                correct_letters.append(guess)
+        else:
+            lives -= 1
+
+    if lives == 0:
+        conn.send(b"LOSE")
+
+    conn.close()
+    server.close()
+
+def multiplayer_join():
+    ip = input("Enter host IP address: ")
+    port = 5555
+
+    client = socket.socket()
+    client.connect((ip, port))
+
+    while True:
+        data = client.recv(1024).decode()
+
+        if data == "WIN":
+            print("You win!")
+            break
+
+        if data == "LOSE":
+            print("You lose!")
+            break
+
+        display, lives = data.split("|")
+        lives = int(lives)
+
+        print("Word: ", display)
+        print("Lives: ", lives)
+        print(stages[lives])
+
+        guess = input("Guess a letter: ").lower()
+        client.send(guess.encode())
+
+    client.close()
+
+#======Multiplayer Entry point=======
+
+
+if mode == "2":
+    choice = input("1.Host game\n2.Join game\nChoose: ")
+
+    if choice == "1":
+        chosen_word = get_random_word()
+        multiplayer_host(chosen_word)
+    else:
+        multiplayer_join()
+
+    exit()
 # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 #  Set 'lives' to equal 6.
 
 lives = 6
+chosen_word = get_random_word()
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
-
-placeholder = ""
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
+placeholder = "_" * len(chosen_word)
 print(placeholder)
 
-game_over = False
 correct_letters = []
+guessed_letters = []
 
-while not game_over:
+while True:
     guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("Already guessed!")
+        continue
+
+    guessed_letters.append(guess)
 
     display = ""
 
@@ -94,17 +204,16 @@ while not game_over:
     #  If lives goes down to 0 then the game should stop and it should print "You lose."
 
     if "_" not in display:
-        game_over = True
-        print("You win.")
+        print("You win!")
+        break
 
 
-    if not guess in chosen_word:
+    if guess not in chosen_word:
         lives -= 1
-        print(lives)
+        print(stages[lives])
 
     # TODO-3: - print the ASCII art from 'stages'
     #  that corresponds to the current number of 'lives' the user has remaining.
-        print(stages[lives])
     if lives == 0:
-        game_over = True
         print("GAME OVER")
+        break
